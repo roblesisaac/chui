af7c4ccb19b66a042bd52b85d8873ad055afe11f
roblesisaac <irobles1030@gmail.com>
2020-05-22 17:10:32 -0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -104,18 +104,19 @@ global.api = new Chain({
       });
     },
     debug: function() {
-      this.next("EVENT:"+this.event);
+      this.next("Method:"+this.method);
     }
   },
   instructions: [
-    "getDbSchema",
-    {
-      if: "routeMethod",
-      get: "relayData",
-      put: "updateItem",
-      post: "sayMethod",
-      delete: "sayMethod"
-    }
+    "debug"
+    // "getDbSchema",
+    // {
+    //   if: "routeMethod",
+    //   get: "relayData",
+    //   put: "updateItem",
+    //   post: "sayMethod",
+    //   delete: "sayMethod"
+    // }
   ]
 });
 global.getDbSchema = new Chain({
