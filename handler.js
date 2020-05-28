223c6e055d026a4713d6b12ed3682d562328ce12
roblesisaac <irobles1030@gmail.com>
2020-05-28 15:13:50 -0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -196,7 +196,7 @@ global.buildSchema = new Chain({
       this.next();
     },
     relayObjs: function() {
-      this.next(this.schema);
+      this.next(this.schemas);
     }
   },
   instructions: [
