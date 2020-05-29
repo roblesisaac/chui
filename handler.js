8730a787c74599fa6d8a93a3397665c7eee20c9e
roblesisaac <irobles1030@gmail.com>
2020-05-28 18:47:55 -0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -102,21 +102,17 @@ global.api = new Chain({
       this.model.findByIdAndUpdate(this.id, this.body, { new: true }).then(function(data){
         self.next(data);
       });
-    },
-    debug: function() {
-      this.next("Method:"+this.method);
     }
   },
   instructions: [
-    "debug"
-    // "getDbSchema",
-    // {
-    //   if: "routeMethod",
-    //   get: "relayData",
-    //   put: "updateItem",
-    //   post: "sayMethod",
-    //   delete: "sayMethod"
-    // }
+    getDbSchema,
+    {
+      if: "routeMethod",
+      get: "relayData",
+      put: "updateItem",
+      post: "sayMethod",
+      delete: "sayMethod"
+    }
   ]
 });
 global.getDbSchema = new Chain({
