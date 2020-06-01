b1e37d6210397e0e61922f634564df844fae48a4
roblesisaac <irobles1030@gmail.com>
2020-06-01 12:44:38 -0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -51,10 +51,10 @@ global.api = new Chain({
     },
     getAllItems: function() {
       var self = this;
-      this.model.find({}, function(err, data){
+      this.model.find(this.filter, function(err, data){
         if(err) return self.error(err);
         self.next(data);
-      });
+      }).limit(50);
     },
     routeMethod: function() {
       this.next(this.method);
