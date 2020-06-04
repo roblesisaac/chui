e5434b32f6109be516ef440058cc53e03c2403f5
roblesisaac <irobles1030@gmail.com>
2020-06-04 12:19:01 -0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -32,7 +32,7 @@ global.authorize = new Chain({
     },
     runChain: function() {
       var self = this;
-      this.ifAuthorized.import(this._memory.storage).start().then(function(memory){
+      this.ifAuthorized.start().then(function(memory){
         self.next("memory.last");  
       });
     },
