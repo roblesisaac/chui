ccf37119e62f86fac980ad3b7f79ba484729cc61
roblesisaac <irobles1030@gmail.com>
2020-06-04 16:58:20 -0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -49,7 +49,7 @@ global.authorize = new Chain({
       });
     },
     userHasToken: function() {
-      this.next(true);
+      this.next(this.query.token == "true");
     },
     tokenIsValid: function() {
       this.next(true);
@@ -96,6 +96,7 @@ global.api = new Chain({
           id: this.arg2,
           filter: {},
           nativeOptions: {
+            token: String,
             limit: Number,
             tailable: null,
             sort: String,
