7288b1682d8aca9dd2908bdd65918784b8ac99f5
roblesisaac <irobles1030@gmail.com>
2020-06-04 12:55:16 -0700

    Update chain.js

--- chain.js
+++ chain.js
@@ -1,5 +1,3 @@
-"use strict";
-
 function addMethodToArray(name, fn) {
   Object.defineProperty(Array.prototype, name, {
     enumerable: false,
@@ -175,9 +173,7 @@ Chain.prototype.import = function(overides, options) {
   var instance = !this._parent
                  ? new Instance(this, overides)
                  : this;
-  if(options.exclude) {
-    instance.memory.exclude(options.exclude);
-  }
+  if(options.exclude) instance.memory.exclude(options.exclude);
   instance.memory.import(overides);
   return instance;
 };
@@ -329,7 +325,7 @@ function Memory(data) {
 }
 Memory.prototype.exclude = function(arr) {
   if(!arr) return;
-  // if(!Array.isArray(arr)) arr = [arr];
+  if(!Array.isArray(arr)) arr = [arr];
   this.exclusions = this.exclusions.concat(arr);
 };
 Memory.prototype.init = function(data) {
