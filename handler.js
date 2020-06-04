1a619a1e5cd8d45b577c34c6bb5e8fa4dec39b66
roblesisaac <irobles1030@gmail.com>
2020-06-04 13:42:25 -0700

    Update handler.js

--- handler.js
+++ handler.js
@@ -35,7 +35,7 @@ global.authorize = new Chain({
           storage = Object.assign({}, this._memory.storage);
       delete storage.protectedChain;
       this.protectedChain.import(storage).start().then(function(memory){
-        self.memory.import(memory);
+        self._memory.import(memory);
         self.next(memory.last);
       });
     },
